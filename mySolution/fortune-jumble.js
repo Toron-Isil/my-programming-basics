6abb486d2d4796b19848260029869e6e1c73e9cf
Toron-Isil <t.isil@arcor.de>
2019-09-04 15:28:35 +0200

    fortune-jumple solved

--- mySolution/fortune-jumble.js
+++ mySolution/fortune-jumble.js
@@ -18,26 +18,40 @@
 
 // Good luck!
 
-const index = Math.floor(Math.random() * (array.length));
+console.log(getFortune());
 
-const job = randomElementPicker(jobs);
+function getFortune() {
+  const jobs = [
+    "an assistant teacher",
+    "a teacher who teaches teachers",
+    "the village computer whisperer",
+    "a space raider",
+    "a camel herder for undercover superheroes",
+    "a mountaineer",
+    "a house servant for a robot",
+    "---pfft work? You've been devoting your life to your passion of miniature crochet"
+  ];
 
+  const job = randomElementPicker(jobs);
 
-const places = ["Berlin", "some tiny island paradise", "wherever semicolons and socks run off to hide", "some hectic cool space city we haven't yet discovered", "the mountains of cyborgtopia", "that place where you used to hang out in a video game", "some stupidly gorgeous coastal town", "the ruins of former Berlin"];
+  const places = [
+    "Berlin",
+    "some tiny island paradise",
+    "wherever semicolons and socks run off to hide",
+    "some hectic cool space city we haven't yet discovered",
+    "the mountains of cyborgtopia",
+    "that place where you used to hang out in a video game",
+    "some stupidly gorgeous coastal town",
+    "the ruins of former Berlin"
+  ];
 
+  const place = randomElementPicker(places);
+
+  return `You'll be celebrating 100 000 GitHub stars on your side project, in the heart of ${place} where you've been working as ${job}!`;
 }
 
 function randomElementPicker(array) {
+  const index = Math.floor(Math.random() * array.length);
 
-return `You'll be celebrating 100 000 GitHub stars on your side project, in the heart of ${place} where you've been working as ${job}!`;
-
-console.log(getFortune());
-    
-function getFortune() {
+  return array[index];
 }
-
-return array[index];
-
-const place = randomElementPicker(places);
-
-const jobs = ["an assistant teacher", "a teacher who teaches teachers", "the village computer whisperer", "a space raider", "a camel herder for undercover superheroes", "a mountaineer", "a house servant for a robot", "---pfft work? You've been devoting your life to your passion of miniature crochet"];
