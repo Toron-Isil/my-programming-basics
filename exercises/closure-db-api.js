20260aab80c7e76b61cd142e138063d73ebdd3cf
Toron-Isil <t.isil@arcor.de>
2019-09-13 15:35:28 +0200

    create vars, named by using

--- exercises/closure-db-api.js
+++ exercises/closure-db-api.js
@@ -4,8 +4,7 @@ const init = () => {
   let createDb = (newEntry) => {
     db.push(newEntry);
 
-    // console.log(db);
-    return `New element '${newEntry}' added! The new length of the array is ${db.length}.`;
+    return `  New element '${newEntry}' added! The new length of the array is ${db.length}.`;
   };
 
   let readDb = () => {
@@ -17,27 +16,32 @@ const init = () => {
 
   let updateDb = (indexNo, newEntry) => {
 
-    if (indexNo >= 0 && indexNo <= (db.length - 1)) {
-      console.log(`Entry '${db[indexNo]}' has been successfully changed to '${newEntry}'. :) `);
+    if (indexNo >= 0 && indexNo < db.length) {
+      console.log(`  Entry '${db[indexNo]}' has been successfully changed to '${newEntry}'. :) `);
       db[indexNo] = newEntry;
     } else {
-      console.log(`ERROR: There is no index '${indexNo}' in the database. '${newEntry}' could not be updated. `);
-
+      console.log(`  ERROR: There is no index '${indexNo}' in the database.`);
+      console.log(`  There was no entry update to '${newEntry}'.`);
+      db.push(newEntry);
+      return `  New element '${newEntry}' has been added!`;
     }
     dbCopy = [...db];
     return dbCopy;
   };
 
   let deleteDb = (indexNo) => {
-    console.log(`Entry '${db[indexNo]}' will be deleted.`);
-    db.splice(indexNo, 1);
-
+    if (indexNo >= 0 && indexNo < db.length) {
+      console.log(`  Entry '${db[indexNo]}' will be deleted.`);
+      db.splice(indexNo, 1);
+    } else {
+      console.log(`  Index '${indexNo}' don't exist.`);
+    }
     dbCopy = [...db];
     return dbCopy;
   };
 
   const api = [];
-  console.log(api);
+  // console.log(api);
 
   api.push(createDb);
   api.push(readDb);
@@ -49,17 +53,23 @@ const init = () => {
   return api;
 };
 
-let createInDb = init();
+const api = init();
 
+let createInDb = api[0];
+let readInDb = api[1];
+let updateDb = api[2];
+let deleteInDb = api[3];
 
-createInDb[0]('Tuna ');
-createInDb[0]('Salad ');
-createInDb[0]('Dressing ');
-createInDb[0]('Onions ');
+createInDb('Tuna ');
+createInDb('Salad ');
+createInDb('Dressing ');
+createInDb('Onions ');
 
-console.log(createInDb[0]('Anchovies '));
-console.log(createInDb[1]());
-console.log(createInDb[2]([3], 'Garlic '));
-console.log(createInDb[3]([4]));
+console.log(createInDb('Anchovies '));
+console.log(readInDb());
+console.log(updateDb([3], 'Garlic '));
+console.log(deleteInDb([4]));
 
-console.log(createInDb[2]([7], 'Pepper '));
+console.log(updateDb([7], 'Pepper '));
+
+console.log(readInDb());
