a89cf09c7e03cebb8c0681b99e25554d35929cab
Toron-Isil <t.isil@arcor.de>
2019-09-26 15:24:12 +0200

    Thursday

--- exercises/lessons/object-ways.js
+++ exercises/lessons/object-ways.js
@@ -28,3 +28,80 @@ console.log("b.name:  '' <- is empty", b.name);
 console.log('c.name:  ', c.name);
 console.log('d.name:  ', d.name);
 console.log(Array.isArray(a.name));
+
+
+//  ==================================================
+
+console.log(' \n ======================================= \n');
+
+// Easy one
+// 1 - Creating objects using the ‘new ’ keyword
+// 2 - Creating objects using literal notation
+// 3 - Creating objects using the object constructor
+// 4 - Adding properties to each object type
+// 5 - Adding methods to each object type
+// ----------------------------------------------------------------------
+
+// Second one
+// change this code to make it working: blush:
+
+// const person;
+// console.log(person.firstName);
+// console.log(person.lastName);
+// console.log(person.age);
+// console.log(person.employed);
+
+// 1.
+
+const person = new Object();
+
+person.firstName = 'Bashira';
+person.lastName = 'Shazam';
+person.birthYear = 1991;
+person.age = '';
+person.employed = 'Fairy';
+person.pets = [];
+
+
+let addPets = (pet, name) => {
+  idxPet = person.pets.length;
+  person.pets[idxPet] = { pet: pet, name: name };
+};
+
+addPets('cat', 'Lilly');
+addPets('parrot', 'Cpt. Hook');
+
+let age = () => {
+  let today = new Date();
+  return today.getFullYear() - person.birthYear;
+};
+age();
+person.age = age();
+
+
+console.log('\t First name: ', person.firstName);
+console.log('\t Last name:  ', person.lastName);
+console.log('\t Age:        ', person.age);
+console.log('\t Employed:   ', person.employed);
+console.log('\t Pets:       ', person.pets);
+console.log('\t Pets:       ', person.pets[0]);
+console.log('\t Pets:       ', person.pets[1]);
+
+// ========================================
+
+Object.defineProperties(personB, {
+
+  firstName: {
+    value: "Matthew"
+  },
+  personB.lastName = 'Pauly',
+  personB.birthYear = 1961,
+  personB.age = '',
+  personB.employed = 'Wizzard',
+  personB.pets = []
+}
+
+  // 1 - change what needed to ADD other more people to your array of objects, you need at lest 5 people
+  // 2 - Find how many people has a job
+  // 3 - display only the people are over 30 and has no job
+
