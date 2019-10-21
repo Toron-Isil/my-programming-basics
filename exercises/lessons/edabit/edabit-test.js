021007781de079fad3c338ea985a51bd2ea9a85f
Toron-Isil <t.isil@arcor.de>
2019-10-21 09:12:52 +0200

    sort edabit

--- exercises/lessons/edabit/edabit-test.js
+++ exercises/lessons/edabit/edabit-test.js
@@ -48,7 +48,7 @@
 // };
 // exerciseTwo();
 
-console.log('********** exerciseThree *********');
+console.log('***********************************');
 
 const exerciseThree = function () {
 
@@ -80,24 +80,21 @@ const exerciseThree = function () {
     followers: 10
   }) // [["likes", 2], ["dislikes", 3], ["followers", 10]]
 
-  console.log('*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* \n')
-
-  console.log(' *-* Keys and Values *-* \n');
-
+  console.log('*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*')
+  console.log('Keys and Values');
   function keysAndValues(obj) {
 
-    let myNewArray = [];
+    let myNewArray = Object.keys(obj);
+    console.log(myNewArray);
 
-    let myKeyArray = Object.keys(obj);
-
-    myValueArray = myKeyArray.map(key => {
-      let newItem = obj[key];
+    myNewArray = myNewArray.map(key => {
+      let newItem = [obj[key]]
+      console.log(newItem)
       return newItem;
-    });
+    })
 
-    myNewArray.push(myKeyArray, myValueArray);
-    console.log('\t obj: \t\t', obj)
-    console.log('\t myNewArray: ', myNewArray);
+    console.log(myNewArray);
+
     return myNewArray;
   }
 
@@ -116,14 +113,14 @@ const exerciseThree = function () {
 
 exerciseThree();
 
-console.log('********** exerciseFour *********');
+console.log('***********************************');
 
 
 const exerciseFour = function () {
 
   function comp(str1, str2) {
     const isTrue = (str1.length === str2.length);
-    console.log(isTrue);
+    // console.log(isTrue);
     return isTrue;
   }
 
@@ -134,7 +131,7 @@ const exerciseFour = function () {
   comp("hello", "edabit") // false
 
 
-  console.log('**** exercise 4b **********')
+  console.log('**** exercise b **********')
 
   function isEqual(num1, num2) {
     const isTrue = (num1 === num2);
@@ -151,82 +148,3 @@ const exerciseFour = function () {
 };
 
 exerciseFour();
-
-console.log('********** exerciseFive *********');
-
-
-const exerciseFive = function () {
-
-  function reverse(arr) {
-    console.log('    ', arr);
-    const reversed = arr.reverse();
-    console.log(' -> ', reversed);
-
-    return reversed;
-  }
-
-  reverse([1, 2, 3, 4]) // [4, 3, 2, 1]
-
-  reverse([9, 9, 2, 3, 4]) // [4, 3, 2, 9, 9]
-
-  reverse([]) // []
-
-  console.log('  ******* exercise 5b *******');
-
-  console.log('   search Index of an Item')
-
-  function search(arr, item) {
-    const indexOfSearched = arr.indexOf(item);
-    console.log(arr, item, ' -> ', indexOfSearched)
-    return indexOfSearched;
-  }
-
-  search([1, 2, 3, 4], 3) // 2
-
-  search([2, 4, 6, 8, 10], 8) // 3
-
-  search([1, 3, 5, 7, 9], 11) // -1
-
-
-};
-
-exerciseFive();
-
-console.log('********** exerciseSix *********');
-
-
-const exerciseSix = function () {
-
-  class Circle {
-    constructor(radius) {
-      this.radius = radius;
-    }
-
-    getArea() { return Math.PI * Math.pow(this.radius, 2) };
-    getPerimeter() { return 2 * Math.PI * this.radius };
-
-  }
-
-  //unquote and use run to test these cases
-  /*let q = new Circle(4.44);
-  console.log(q.getArea());
-  console.log(q.getPerimeter());
-  */
-
-  let circy = 0;
-  circy = new Circle(11);
-  circy.getArea();
-  console.log('get Area \t of radius   11 : ', circy.getArea());
-  // Should return 380.132711084365
-
-  circy = new Circle(4.44);
-  circy.getPerimeter();
-  console.log('get Perimeter \t of radius 4.44 : ', circy.getPerimeter());
-
-  // Should return 27.897342763877365
-
-};
-
-exerciseSix();
-
-console.log('*********** END ************');
